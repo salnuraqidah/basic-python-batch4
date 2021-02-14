9446e00a428cef186a5384b5ce633f26c81306ae
salnuraqidah <salnuraqidah@gmail.com>
2021-02-14 08:59:45 +0700

    update casting

--- casting.py
+++ casting.py
@@ -1,17 +1,37 @@
-a = 2.8
+# float to int
+x = 1.9999999
+print(x)
+print(type(x))
+
+y = int(x)
+print(y)
+print(type(y))
+
+
+# int to float
+a = 100
+print(a)
 print(type(a))
-# casting int
-b = int(a)
-print(type(b))
+
+b = float(a)
 print(b)
+print(type(b))
 
-# casting float
-c = 2
-d = float(c)
-print(type(d))
 
-# casting string
-c = '2'
-d = float(c)
-print(d)
-print(type(d))
+# str to float
+x = "4.5"
+print(x)
+print(type(x))
+
+y = float(x)
+print(y)
+print(type(y))
+
+# float to str
+a = 9.9
+print(a)
+print(type(a))
+
+b = str(a)
+print(b)
+print(type(b))
